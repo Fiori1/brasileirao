f4b9af4219fb691f428f3009e14ccc456f88db5f
Fiori1 <enrico009g9@gmail.com>
2023-12-05 22:04:17 -0300

    05/12

--- assets/js/tabela.js
+++ assets/js/tabela.js
@@ -7,3 +7,17 @@ function createGame(time1, hora, time2){
     </li>
     `
 }
+let delay = -0.4;
+
+function createCard(data, dia, jogos){
+    return ` 
+    <div class="card" style="animation-delay: ${delay}s">
+    <h2>${data} <span>${dia}</span></h2>
+    <ul>
+    ${jogos}
+    </ul>
+    </div>
+    `
+}
+document.querySelector("#cards").innerHTML =  
+createCard("24/11", "quinta", createGame("santos", "13:00", "palmeiras"))
